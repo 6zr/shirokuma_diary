4076641ca02749083efc5b23558bd04a0e086e00
6zr <actsofgolgom@gmail.com>
2025-06-29 06:44:27 +0900

    値の受け渡し方法

--- scripts/output.js
+++ scripts/output.js
@@ -1,5 +1,8 @@
-const core = require("@actions/core");
+// const core = require("@actions/core");
 // const github = require("@actions/github");
 
-const keywords = core.getInput("keywords");
-console.log(keywords);
+// const keywords = core.getInput("keywords");
+// console.log(keywords);
+
+console.log(process.env.STEP_OUTPUT_KEYWORDS);
+console.log(process.env.STEP_OUTPUT_DIARY);
