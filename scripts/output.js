6948b591ef8e6dc556ea8830d3501da81ee46144
6zr <actsofgolgom@gmail.com>
2025-06-29 07:04:20 +0900

    output試す

--- scripts/output.js
+++ scripts/output.js
@@ -10,6 +10,9 @@ const dayOfWeek = new Intl.DateTimeFormat('ja-JP', { weekday: 'short' }).format(
 const shortDayOfWeek = dayOfWeek.replace('曜日', ''); // '月曜日' -> '月'
 const TODAY = `${year}/${month}/${day}(${shortDayOfWeek})`;
 
+const KEYWORDS = process.env.STEP_OUTPUT_KEYWORDS;
+const MARKOV = process.env.STEP_OUTPUT_MARKOV;
+
 const outputDir = './diary';
 if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
@@ -19,9 +22,17 @@ if (!fs.existsSync(outputDir)) {
 const filename = 'index.md';
 const outputPath = path.join(outputDir, filename);
 
-console.log(process.env.STEP_OUTPUT_KEYWORDS);
-console.log(process.env.STEP_OUTPUT_DIARY);
-
-const diary = `[${TODAY}]\n\n${result}\n\n...ってかんじの日だったワン`;
+const diary = `[${TODAY}]\n\n${MARKOV}\n\n...ってかんじの日だったワン`;
 console.log(diary);
 fs.writeFileSync(outputPath, diary);
+
+const keywords = KEYWORDS
+    .map(x => ({
+        keyword: x.keyword.name, 
+        category: x.keyword.keywordCategory.code,
+    }))
+    .filter(x => !x.keyword.includes('{'))
+    .map(x => `${x.category} => ${x.keyword}`)
+    .join('\n');
+;
+console.log(keywords);
