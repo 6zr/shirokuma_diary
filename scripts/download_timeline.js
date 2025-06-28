e25b24f464c19be4c4486c6719254de1b210f70d
6zr <actsofgolgom@gmail.com>
2025-06-28 16:47:23 +0900

    取得するデータをユーザーtimelineにするのと、content抽出してみる

--- scripts/download_timeline.js
+++ scripts/download_timeline.js
@@ -18,12 +18,16 @@ const M = new Mastodon({
 // ファイルを保存するディレクトリのパス
 const outputDir = './data'; // または path.join(__dirname, '../data'); など、適切なパスを設定
 
-M.get('timelines/home', {}) // ホームタイムラインの例
+
+// M.get('timelines/home', {limit: 40}) // ホームタイムラインの例
+M.get('accounts/898916/statuses', {limit: 40}) // ホームタイムラインの例
     .then(resp => {
         const timeline = resp.data;
         const timestamp = new Date().toISOString().replace(/:/g, '-').replace(/\..+/, ''); // ミリ秒を除去
-        const filename = `timeline-${timestamp}.json`;
-        const outputPath = path.join(outputDir, filename); // path.joinでパスを結合
+        const rawFilename = `timeline-${timestamp}.json`;
+        const contentsFilename = `contents.json`;
+        const rawOutputPath = path.join(outputDir, rawFilename); // path.joinでパスを結合
+        const contentsOutputPath = path.join(outputDir, contentsFilename);
 
         // ここが重要: ディレクトリが存在しない場合は作成する
         // { recursive: true } を指定することで、途中のディレクトリも全て作成される
@@ -32,8 +36,12 @@ M.get('timelines/home', {}) // ホームタイムラインの例
             console.log(`Directory created: ${outputDir}`);
         }
 
-        fs.writeFileSync(outputPath, JSON.stringify(timeline, null, 2));
-        console.log(`Timeline saved to ${outputPath}`);
+        const contents = timeline.map(t => (t == null) ? '' : (t.content || ''));
+        const contentsText = contents.join('\n');
+
+        fs.writeFileSync(rawOutputPath, JSON.stringify(timeline, null, 2));
+        fs.writeFileSync(contentsOutputPath, contentsText);
+        console.log(`Timeline saved to ${rawOutputPath}, ${contentsOutputPath}`);
     })
     .catch(err => {
         console.error('Error downloading timeline:', err);
